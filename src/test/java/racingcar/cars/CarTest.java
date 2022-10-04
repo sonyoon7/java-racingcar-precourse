a0aef9237b26eef169c1a1398e8f6b8516464afb
sonyoon7 <sonyoon7@gmail.com>
2022-10-04 23:40:17 +0900

    test(Car): 자동차 도데인 테스트

---
+++ src/test/java/racingcar/cars/CarTest.java
@@ -0,0 +1,48 @@
+package racingcar.cars;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class CarTest {
+
+    private static Car pobiCar;
+
+    @BeforeAll
+    public static void 자동차_세팅 () throws Exception {
+        pobiCar = new Car("pobi");
+    }
+
+    @Test
+    @DisplayName("자동차 이름은 5자 이하만 유효합니다.")
+    public void 자동차_이름_유효성() throws Exception {
+        assertAll(
+                () -> assertThatExceptionOfType(Exception.class)
+                        .isThrownBy(() -> new Car("일이삼사오육칠"))
+                        .withMessageMatching("자동차 이름은 \\d+자 이하만 가능하다."));
+    }
+
+    @Test
+    @DisplayName("자동차 이름")
+    public void 자동차_이름_가져오기() throws Exception {
+
+        assertThat(pobiCar.getName()).isEqualTo("pobi");
+    }
+
+    @Test
+    @DisplayName("자동차 위치")
+    public void 자동차_위치_세팅() throws Exception {
+        pobiCar.setLocation(1);
+        assertThat(pobiCar.getLocation()).isEqualTo(1);
+        pobiCar.setLocation(1);
+        assertThat(pobiCar.getLocation()).isEqualTo(2);
+        pobiCar.setLocation(-1);
+        assertThat(pobiCar.getLocation()).isEqualTo(1);
+        pobiCar.setLocation(0);
+        assertThat(pobiCar.getLocation()).isEqualTo(1);
+    }
+
+}
