bdb83c049883f7ff3dd05576bb38f85719845f52
sonyoon7 <sonyoon7@gmail.com>
2022-10-05 23:30:10 +0900

    refactor(Rounds): 마지막 라운드 호출 추가

--- src/main/java/racingcar/rounds/Rounds.java
+++ src/main/java/racingcar/rounds/Rounds.java
@@ -7,15 +7,23 @@ import java.util.List;
 
 public class Rounds {
 
-    private List<Round> rounds;
+    private static List<Round> rounds = new ArrayList<>();
 
     public Rounds(List<Round> rounds) {
         this.rounds = rounds;
     }
 
-    public Rounds playRacingCars(Cars cars , int currentRound){
-        ArrayList<Round> rounds = new ArrayList<>();
-        Round round = new Round(cars.race(), currentRound );
+    public List<Round> getRounds() {
+        return rounds;
+    }
+
+    public static Rounds playRacingCars(Cars cars, int currentRound) {
+        rounds.add(new Round(cars.race(), currentRound));
+        System.out.println();
         return new Rounds(rounds);
+    }
+
+    public static Round getLastRound() {
+        return rounds.get(rounds.size()-1);
     }
 }
