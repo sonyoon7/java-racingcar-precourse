5feb4045dea5782c9ebef9f84ea36ca3a7f67d5d
sonyoon7 <sonyoon7@gmail.com>
2022-10-05 23:33:33 +0900

    feat(Car): 레이스결과 프린트 기능 추가, Comparable 구현 추가

--- src/main/java/racingcar/cars/Car.java
+++ src/main/java/racingcar/cars/Car.java
@@ -1,6 +1,6 @@
 package racingcar.cars;
 
-public class Car {
+public class Car implements Comparable<Car>{
 
     private static final int MAX_OF_NAME_LENGTH = 5;
     private static final int MOVE = 1;
@@ -26,10 +26,22 @@ public class Car {
         if (isForward) this.location += MOVE;
     }
 
+    public void printRaceResult() {
+        System.out.print(this.name+" : ");
+        for (int i = 0; i < this.location; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
     private void checkValidation(String name) throws Exception {
         if (name.length() > MAX_OF_NAME_LENGTH) {
             throw new Exception("자동차 이름은 "+ MAX_OF_NAME_LENGTH + "자 이하만 가능하다.");
         }
     }
 
+    @Override
+    public int compareTo(Car c) {
+        return Integer.compare(this.location, c.getLocation());
+    }
 }
