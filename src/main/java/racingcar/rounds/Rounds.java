80e294b4ad901fb89fc2da6ee673d321738c535e
sonyoon7 <sonyoon7@gmail.com>
2022-10-05 21:14:53 +0900

    refactor(Round): 라운드 자동차 레이스 구현 코드 수정

--- src/main/java/racingcar/rounds/Rounds.java
+++ src/main/java/racingcar/rounds/Rounds.java
@@ -1,6 +1,5 @@
 package racingcar.rounds;
 
-import racingcar.cars.Car;
 import racingcar.cars.Cars;
 
 import java.util.ArrayList;
@@ -14,20 +13,9 @@ public class Rounds {
         this.rounds = rounds;
     }
 
-    /*
-    TODO
-    1. 랜덤 수 전진 후진 판단
-    2. 자동차 위치 업데이트
-    3. Round 객체에 담기
-    */
     public Rounds playRacingCars(Cars cars , int currentRound){
         ArrayList<Round> rounds = new ArrayList<>();
-
-        for (Car car : cars.getCars()) {
-
-        }
-        Round round = new Round(cars, currentRound );
-
+        Round round = new Round(cars.race(), currentRound );
         return new Rounds(rounds);
     }
 }
