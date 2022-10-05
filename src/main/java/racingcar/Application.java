274fbee7afaa2bd6ef5f02224d8ec27881480e2c
sonyoon7 <sonyoon7@gmail.com>
2022-10-06 00:27:15 +0900

    feat(Application): 게임 실행 코드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.game.Game;
+import racingcar.message.Message;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        try {
+            Game.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+//            System.out.println(Message.INPUT_ERROR.getMessage());
+        }
     }
 }
